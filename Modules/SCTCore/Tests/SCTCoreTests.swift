9080bd933c0e7040eb0412697dca55c61cb08d0d
song.yue <yuesong@gmail.com>
2026-04-07 15:00:49 +0800

    chore: setup root Swift package and SCTCore module placeholder

---
+++ Modules/SCTCore/Tests/SCTCoreTests.swift
@@ -0,0 +1,8 @@
+import XCTest
+@testable import SCTCore
+
+final class SCTCoreTests: XCTestCase {
+    func testExample() throws {
+        XCTAssertTrue(true)
+    }
+}
---
+++ Package.swift
@@ -0,0 +1,14 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "splitspeak",
+    platforms: [.iOS(.v17)],
+    products: [
+        .library(name: "SCTCore", targets: ["SCTCore"])
+    ],
+    targets: [
+        .target(name: "SCTCore", path: "Modules/SCTCore/Sources"),
+        .testTarget(name: "SCTCoreTests", dependencies: ["SCTCore"], path: "Modules/SCTCore/Tests")
+    ]
+)
